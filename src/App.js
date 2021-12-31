58a8b6b5af3636ddb11d8bfde23ca904a1d798d0
LinKzorD <89782983+LinKzorD@users.noreply.github.com>
2021-12-31 17:58:47 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useState } from "react";
+import { array } from "./array";
+import "./App.css";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todos: [
+          ...state.todos,
+          {
+            id: new Date().valueOf(),
+            text: action.text,
+            date: action.date,
+            completed: false,
+          },
+        ],
+      };
+    case "del-todo":
+      return {
+        todos: state.todos.map((t, idx) =>
+          idx === action.idx ? { ...t, completed: !t.completed } : t
+        ),
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [{ todos }, dispatch] = useReducer(reducer, { todos: array });
+  const [text, textSet] = useState("");
+  const [date, dateSet] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>TODO LIST:</h1>
+      <div className='todo'>
+        {todos.map((todo, idx) => {
+          if (!todo.completed) {
+            return (
+              <div key={todo.id} className='todolist'>
+                <div className='todolistele'>
+                  <div>
+                    <h2>{todo.text}</h2>
+                  </div>
+                  <div>
+                    <p> {todo.date} </p>
+                  </div>
+                  <button
+                    type='button'
+                    className='btn'
+                    onClick={() => dispatch({ type: "del-todo", idx })}
+                  >
+                    Done
+                  </button>
+                </div>
+              </div>
+            );
+          }
+        })}
+      </div>
+      <div className='form-div'>
+        <h3>Add task:</h3>
+        <form className='form'>
+          <input
+            value={text}
+            placeholder='Todo'
+            onChange={(e) => textSet(e.target.value)}
+          />
+          <input
+            value={date}
+            placeholder='Date'
+            onChange={(e) => dateSet(e.target.value)}
+          />
+          <button
+            className='btn'
+            onClick={(e) => {
+              e.preventDefault();
+              dispatch({ type: "add-todo", text, date });
+              textSet("");
+              dateSet("");
+            }}
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ src/array.js
@@ -0,0 +1,20 @@
+export const array = [
+  {
+    id: 1,
+    text: "Clean dishes",
+    date: "01-01-2022",
+    completed: false,
+  },
+  {
+    id: 2,
+    text: "Clean house",
+    date: "03-01-2022",
+    completed: false,
+  },
+  {
+    id: 3,
+    text: "Study",
+    date: "05-01-2022",
+    completed: false,
+  },
+];
